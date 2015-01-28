882d02764a5b5f0e9235edd600f7d37ca9380bd1
R1kison <airat@flyap.ru>
2015-01-28 19:56:58 +0400

    r1k

--- app/src/main/java/su/pfm/utils/PlayerViewListAdapter.java
+++ app/src/main/java/su/pfm/utils/PlayerViewListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -46,7 +47,15 @@ public class PlayerViewListAdapter extends BaseAdapter {
         if (view == null) {
             view = mInflater.inflate(R.layout.one_player, parent, false);
         }
+        RelativeLayout playerBlock = (RelativeLayout) view.findViewById(R.id.player_block);
+        switch(mPlayers.get(position).position) {
+            case "GK": playerBlock.setBackground(parent.getContext().getResources().getDrawable(R.drawable.one_player_gk)); break;
+            case "DEF": playerBlock.setBackground(parent.getContext().getResources().getDrawable(R.drawable.one_player_def)); break;
+            case "MID": playerBlock.setBackground(parent.getContext().getResources().getDrawable(R.drawable.one_player_mid)); break;
+            case "FOR": playerBlock.setBackground(parent.getContext().getResources().getDrawable(R.drawable.one_player_for)); break;
+        }
         TextView nameText = (TextView) view.findViewById(R.id.op_name);
+
         nameText.setText(mPlayers.get(position).name);
         TextView rateText = (TextView) view.findViewById(R.id.op_rate);
         rateText.setText(String.valueOf(mPlayers.get(position).plevel) );
